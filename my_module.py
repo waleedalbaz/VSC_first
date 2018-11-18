615a06a787e96fd7b426b958d9a67b1e2155a5a3
Waleed <walllaid@gmail.com>
2018-11-18 20:10:49 +0300

    Add TestSuite class for testing

--- my_module.py
+++ my_module.py
@@ -1,8 +1,5 @@
 """
 This my stuff.. You know :)
-I want to disturb this to git
-
-another try
 """
 class _Node():
     def __init__(self, data = None):
@@ -111,3 +108,47 @@ def traverse_grid(a_list, start_cell, direction, num_steps):
         col = start_cell[1] + step * direction[1]
         print "Processing cell", (row, col), 
         print "with value", a_list[row][col]
+
+
+
+# ================= Test class =============
+"""
+Lightweight testing class inspired by unittest from Pyunit
+https://docs.python.org/2/library/unittest.html
+Note that code is designed to be much simpler than unittest
+and does NOT replicate unittest functionality
+"""
+
+class TestSuite:
+    """
+    Create a suite of tests similar to unittest
+    """
+    def __init__(self):
+        """
+        Creates a test suite object
+        """
+        self.total_tests = 0
+        self.failures = 0
+    
+    
+    def run_test(self, computed, expected, message = ""):
+        """
+        Compare computed and expected
+        If not equal, print message, computed, expected
+        """
+        self.total_tests += 1
+        if computed != expected:
+            msg = message + " Computed: " + str(computed)
+            msg += " Expected: " + str(expected)
+            print msg
+            self.failures += 1
+    
+    def report_results(self):
+        """
+        Report back summary of successes and failures
+        from run_test()
+        """
+        msg = "Ran " + str(self.total_tests) + " tests. "
+        msg += str(self.failures) + " failures."
+        print msg
+               
